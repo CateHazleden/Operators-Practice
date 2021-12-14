3b040b3b718c7c2e177c041caa8243f994ca7845
CateHazleden <catehazleden@gmail.com>
2021-12-14 21:20:03 +1100

    Session 21 query selector commit

--- operators.js
+++ operators.js
@@ -11,24 +11,34 @@ console.log(discountPrice);
 
 
 //EXERCISE_2 - Conditional Operators - 3 cat show breeds per group - number of breeds needed to complete group - modulus
+*/
+
+var breedCount = document.getElementsByTagName('li');
+
+//console.log("breedCount"); 
 
 
-var breedCount = document.getElementsByTagName("LI");
+//TRY THIS >>>
 
-console.log("breedCount"); 
+function breedFunction() { 
+    let breedCount = number(document.getElementsByTagName('li'));
+
+    let b = number(breedCount % 3);
+
+    document.getElementById('modulus').innerHTML = (b == 0 ? 0:(3-b));
+
+};
 
 
-TRY THIS >>> function breedFunction() { 
-    let breedCount = number(document.getElementsByTagName("LI");
-    let breedsNeeded = number(breedCount % 3)
+
 
 
 //^ counts number of breeds in list - can't get it to work!!
 
-var num = document.querySelectorAll(".groups.li").length;
-console.log(num)
+/*let num = document.querySelectorAll(".groups.li").length;
+console.log(num);
 
-var breedsNeeded = num % 3
+let breedsNeeded = num % 3;
 
 console.log(breedsNeeded == 0 ? 0:(3-breedsNeeded));
 
@@ -63,18 +73,18 @@ function validateForm() {
     document.getElementById("paraMessage").innerText = defineMessage
 
 }
-
+*/
 
 //EXERCISE_4 - ternary operators
 
-const val = "Cate";
+/*const val = "Cate";
 const rainPercentage = 0.2;
-const temperatureInC = 27;
+const temperatureInC = 21;
 
 let message = `Hello, ${val}, with a rain chance of ${rainPercentage * 100}% and a temperature of ${temperatureInC}C we recommend that you ` + (rainPercentage > 0.3 ? "take an umbrella" : "enjoy this rain-free day") +
 `${temperatureInC >= 26 ? ' and watch out for heatstroke.' : ' and bask in this fine weather.'}`;
 
-console.log(message);
+
 
 document.getElementById("messageEx4").innerText = message;
 
@@ -96,10 +106,10 @@ else {hours >=19
     alert("good evening")
 }
 
-
+*/
 //SESSION 12b - Sequence and comments
 
-const lineOne = "Joy to the world";
+/*const lineOne = "Joy to the world";
 const lineTwo = "All the boys and girls";
 //const lineThree = "Joy to the fishes in the deep blue sea";
 const lineFour = "Joy to you and me";
@@ -108,11 +118,11 @@ const lineFour = "Joy to you and me";
 const chorus = `${lineOne}\n${lineTwo}\n${lineFour}`;
 
 chorus;
-
+*/
 
 //SESSION 13 - 3 variable demographics - 1st kids, 2nd teens, 3rd adults 1 - print 
 
-function checkMessage() {
+/*function checkMessage() {
 
     let age;
     let returnMessage;
@@ -138,7 +148,7 @@ function checkMessage() {
 }
 
 
-/*let messageEx7 = `Hello, ${nameVal}, with a rain chance of ${rainPercentage * 100}% and a temperature of ${temperatureInC}C we recommend that you ` + (rainPercentage > 0.3 ? "take an umbrella" : "enjoy this rain-free day") +
+let messageEx7 = `Hello, ${nameVal}, with a rain chance of ${rainPercentage * 100}% and a temperature of ${temperatureInC}C we recommend that you ` + (rainPercentage > 0.3 ? "take an umbrella" : "enjoy this rain-free day") +
 `${temperatureInC >= 26 ? ' and watch out for heatstroke.' : ' and bask in this fine weather.'}`;
 
 console.log(messageEx7);
@@ -146,7 +156,7 @@ console.log(messageEx7);
 
 //SESSION 14a SWITCH STATEMENTS
 
-function catGreetings(){
+/*function catGreetings(){
 
         
         let breedVal = document.getElementById("sessionFourteena").value; 
@@ -162,7 +172,7 @@ function catGreetings(){
                 break;
         
             case  "Devon Rex":
-                greeting = "Oh.My.Gosh! Your cat has the tightest curls and they are wonderful!";
+                greeting = "Oh.My.Gosh! Your cat has the tightest curls. They are wonderful!";
                 break;
             
             case  "Abyssinian":
@@ -224,15 +234,19 @@ function checkMessage() {
     }
     document.getElementById("demoPara").innerText = demoMessage;
 }
-
+    
+function demoReset() {
+    document.getElementById("demoReset")
+}
 //Session 14c Loops
 
 //Increment
-/*let count = 1
+let count = 1
 //while(count <=5) {
 //console.log(`I am the ${count}, I love to count!`)
 count++;
 }*/
+/*
 //Decrement
 let dec = 10
 while(dec >=1) {
@@ -279,41 +293,72 @@ document.getElementById("btn").addEventListener("click", function() {
     document.getElementById("numMessage").innerHTML = "The answers you seek can not be revealed. <br> ";
     }
 );
+
+
 document.getElementById("numMessage").addEventListener("mouseover", function(){
     document.getElementById("secretMess").innerHTML = "They are in the console log though ;)"
-}
+    }
 );
 
-/*document.getElementById("numMessage" + "secretMess").addEventListener("mouseout", function(){
-    $(this).hide();
-})
+let hides = document.getElementById("hideme");
+
+for ( let i=0; i < hides.length; i++) {
+    hides[i].addEventListener("mouseover", function(){
+     let id = this.getAttribute("secretMess");
+     document.getElementById(hideme).style.visibility = "hidden";
+     });
+ }
+
+for ( let i=0; i < hides.length; i++) {
+   hides[i].addEventListener("mouseout", function(){
+    id = this.getAttribute("secretMess");
+    document.getElementById(hideme).style.visibility = "hidden";
+    });
+}
 */
+
+/*
 //SESSION 15
 
 //Session 15a Functions, Arguments, Parameters, Return
 
-function myFunction(a,b) {
+document.getElementById("returner").innerHTML = myFunction(5+10);
+
+function myFunction(a, b) {
     return a + b;
 }
 
-document.getElementById("returner").innerHTML = myFunction(10,5);
 
 
 
-let hello = "Hi"
-let firstName = "PonyFace"
+
+let hello = "Hi";
+let firstName = "PonyFace";
 
 function say(hello, firstName) {        
     return `${hello}, ${firstName}!`
   };
 
- document.getElementById("returnGN").innerHTML = say(hello, firstName);
+document.getElementById("returnGN").innerHTML = say(hello, firstName);
 
 
 
 
-//Session 16a Arrays
+//Session 15b Arrays - push pop shift
 
+const cars = ["Mini", "Fiat", "Morris", "Hunter", "VW", "Skode", "Daihatsu"];
+
+console.log(cars);
+
+cars.push("Ford");
+
+document.getElementById("cB1").addEventListener("click", carFunction);
+
+function carFunction() {
+    document.getElementById("pushEx").innerHTML = cars;
+}
+
+console.log(carFunction);
 
 const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
@@ -336,7 +381,7 @@ days;
 const primes = [2,3,5,7]
 
 
-/*const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
 days.slice(2, 5);
 
@@ -346,32 +391,190 @@ const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
 days.splice(2);
 // => ["Wed", "Thu", "Fri", "Sat", "Sun"]
-*/
+*//*
 const menu = ['Jalapeno Poppers', 'Cheeseburger', 'Fish and Chips', 'French Fries', 'Onion Rings'];
 
 menu.splice(1, 2, 'Veggie Burger', 'House Salad', 'Teriyaki Tofu');
 // => ["Cheeseburger", "Fish and Chips"]
 
-menu;
+menu;*/
 // => ["Jalapeno Poppers", "Veggie Burger", "House Salad", "Teriyaki Tofu", "French Fries", "Onion Rings"]
 
-//Session 16b
+//Session 15b
 //Arrays
 
-const cars = ["Mini", "Fiat", "Morris", "Hunter", "VW", "Skode", "Daihatsu"]
+/*
 
-/*for (i=0; i< cars.length; i++){
+for (let i=0; i< cars.length; i++){
+    //console.log(`My Favourite Car is ${cars[i]}`);
+    console.log(`My Favourite Car Number ${i} is ${cars[i]}`);
+}
+
+
+for (let i=cars.length-1; i>=0; i--){
+    //console.log(`My Favourite Car is ${cars[i]}`);
+    console.log(`My Favourite Car Number ${i} is ${cars[i]}`)
+}
+
+for (let i=0; i<cars.length; i++){
     //console.log(`My Favourite Car is ${cars[i]}`);
     console.log(`My Favourite Car Number ${i} is ${cars[i]}`)
 }
 */
 
-for (i=cars.length-1; i>=0; i--){
-    //console.log(`My Favourite Car is ${cars[i]}`);
-    console.log(`My Favourite Car Number ${i} is ${cars[i]}`)
+
+/*
+const copyOfCars = [...cars];
+
+let x = copyOfCars
+
+console.log(x)
+document.getElementById("carsPara").innerHTML = "<p>This is a Copy of Cars List:" + " " + x + "</p>";
+
+// Work out how to space-separate values in output
+
+
+const allCars = [...cars, 'Austin', 'Leyland']
+
+let i = allCars
+
+console.log(i)
+document.getElementById("carsPara2").innerHTML = "<p>This is a Spread of Cars List with two cars added:" + " " + i + "</p>";
+
+let q = cars.pop();
+document.getElementById("pop").addEventListener("click", popFunction);
+
+function popFunction() {
+    document.getElementById("popPara").innerHTML = q;
+};
+
+
+//Session_18 Objects and Arrays
+
+
+
+const myArray = [];
+
+myArray.summary = 'Empty array!';
+
+myArray.push(2, 3, 5, 7);
+
+
+//Object Iteration
+
+const myArray1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+
+for (const inc of myArray1) {
+  console.log(inc);
 }
 
-for (i=0; i<cars.length; i++){
-    //console.log(`My Favourite Car is ${cars[i]}`);
-    console.log(`My Favourite Car Number ${i} is ${cars[i]}`)
+const myArrayOfCats = ['Max', 'Bertie', 'Teddy', 'Cpt Midnight', 'Philip'];
+
+for (const inc of myArrayOfCats){
+    document.getElementById("catArray").innerHTML = inc;
 }
+
+
+const address = {
+    street1: '11 Broadway',
+    street2: '2nd Floor',
+    city: 'New York',
+    state: 'NY',
+    zipCode: "10004"
+  };
+  
+  for (const key in address) {
+    console.log(key);
+  }
+
+
+
+function changeBG0(){
+    document.getElementById("redbtn").style.backgroundColor = "#8e2f2d";
+}  
+
+function changeBG1(){
+    document.getElementById("pinkbtn").style.backgroundColor = "pink";
+}  
+
+function changeBG2(){
+    document.getElementById("beigebtn").style.backgroundColor = "beige";
+} 
+
+document.getElementById("changeBG").onclick = changeColor
+
+  function changeColor() {
+    document.getElementById(redbutt).style.backgroundColor = "#8e2f2d";
+
+    console.log()
+
+    document.getElementById("pinkButt").style.backgroundColor = "#9c645c";
+
+    document.getElementById("beigeButt").style.backgroundColor = "#f2d8b3";
+}
+
+//Session_19
+
+function changeFontColor() {
+    let fontColor = document.getElementById("colorInput").value;
+
+    document.getElementById('fontChangePara').style.color = fontColor;
+}
+
+function changeFontColor() {
+    let fontColor = document.getElementById("colorInput").value;
+
+    document.getElementsByTagName('P').style.color = fontColor;  
+};
+
+//session_21
+
+
+let listItems = document.querySelector('ul');
+console.log(listItems);*/
+
+/*let listItems = document.querySelectorAll('ul');
+console.log(listItems);
+
+let classItems = document.getElementsByClassName('sesssion_19');
+console.log(classItems);*/
+
+
+let idItems = document.getElementById('colorInput');
+console.log(idItems);
+
+
+let classItems2 = document.getElementsByClassName('border');
+console.log(classItems2);
+
+let qSelect = document.querySelector('p');
+console.log(qSelect);
+
+let qSelectA = document.querySelectorAll('p');
+console.log(qSelectA);
+
+let tName = document.getElementsByTagName('p');
+console.log(tName);
+
+//tName.style.color = 'red';
+
+
+/*for (let i =0; i<classItems2.length; i++){
+    classItems2[i].style.color = 'red';
+}*/
+
+//Kruti's code
+
+function changeBGandFont() {
+    
+for (let i =0; i<classItems2.length; i++){
+    classItems2[i].style.color = 'red';
+    classItems2[i].innerHTML = "Paragraph text changed";
+};
+
+for (let i = 0; i < classItems2.length; i++){
+    classItems2[i].style.backgroundColor = 'purple';
+};
+
+};
+
